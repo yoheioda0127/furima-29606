a775faf41cc4f6b682a547cee63ac7189be36851
yoheioda0127 <maikemi00romans@gamil.com>
2020-11-06 06:55:38 +0900

    userモデルのテストコード記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_030158) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
