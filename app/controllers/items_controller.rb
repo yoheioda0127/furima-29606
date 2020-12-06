e9489a5f9deafcd56852ba93c65ef67eaf692cb6
yoheioda0127 <maikemi00romans@gamil.com>
2020-12-06 17:53:45 +0900

    コントローラーの記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -34,6 +34,13 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to action: :index
+    end
+    
   end
 
   private
