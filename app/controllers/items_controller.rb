d7480872b427e5a42a78d334e6ac18ee8b3ab10b
yoheioda0127 <maikemi00romans@gamil.com>
2020-12-06 17:13:03 +0900

    プルリクエスト修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,8 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to action: :show
     else
       render :edit
