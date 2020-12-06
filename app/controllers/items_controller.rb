18f0c575a34a44cd7c2488b8dc2e3b23c1358a4a
yoheioda0127 <maikemi00romans@gamil.com>
2020-12-06 12:56:43 +0900

    コントローラーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,21 @@ class ItemsController < ApplicationController
     end
   end
   
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to action: :show
+    else
+      render :edit
+    end
+
+  end
+
   def show
     @item = Item.find(params[:id])
   end
